74830203c974bbc32ec41c9dd18a16518be15271
Pablo Cuenca <108037606+cuencapablo@users.noreply.github.com>
2024-04-06 09:51:19 -0500

    Update dwmblocks config

--- dwmblocks/config.h
+++ dwmblocks/config.h
@@ -1,15 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-  // {"⌨ ",   "kbselect",            0,                30},
-	/* {"",	   "music",	              1,               	12}, */
-	{"",	   "volume",	            0,               	10},
-	{"",	   "battery",	            3,               	 3},
-	/* {"",	   "clock",	             30,               	 1}, */
-	/* {"",	   "tgredshift",	        0,               	15}, */
-	{"",	   "brightness",	      0,               	 5},
-	{"",	   "internet",	          5,               	 4},
+    /*Icon*/    /*Command*/    /*Update Interval*/ /*Update Signal*/
+    {"",	"kbselect",	        0,	            30  },
+    {"",	"volume",	          0,	            10  },
+    {"",	"battery",	        3,	            3  },
+    {"",	"brightness",	      0,	            5  },
+    {"",	"internet",	        5,	            4  },
 };
 
-//Sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char *delim = "  ";
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char *delim = " | ";
+static unsigned int delimLen = 5;
